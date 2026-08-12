b02ed42e5e31294e8c3a019b0fb31d45d790601a
Andrés Muñoz <andres2078@gmail.com>
2026-08-12 15:07:49 -0500

    chore: configure environment variable for API key

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,23 +1,19 @@
-import axios from "axios"
-import type { SearchType } from "../types"
+import axios from 'axios';
+import type { SearchType } from '../types';
 
 export default function useWeather() {
-
   const fetchWeather = async (search: SearchType) => {
-    
-    const appId = ''
+    const appId = import.meta.env.VITE_API_KEY;
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
-
-      const {data} = await axios(geoUrl)
-      console.log(data)
-
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const { data } = await axios(geoUrl);
+      console.log(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return {
-    fetchWeather
-  }
+    fetchWeather,
+  };
 }
