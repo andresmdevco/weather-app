b76d555ba131af007082e56542f7b93fbe86307b
Andrés Muñoz <andres2078@gmail.com>
2026-08-12 14:50:24 -0500

    feat: make first weather API request

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import Form from './components/Form/Form';
 import useWeather from './hooks/useWeather';
 
 function App() {
-
-  const { fetchWeather } = useWeather()
+  const { fetchWeather } = useWeather();
 
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
 
       <div className={styles.container}>
-        <Form 
-          fetchWeather={fetchWeather}
-        />
+        <Form fetchWeather={fetchWeather} />
         <p>2</p>
       </div>
     </>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,7 @@ import styles from './Form.module.css';
 import Alert from '../Alert/Alert';
 
 type FormProps = {
-  fetchWeather: () => void;
+  fetchWeather: (search: SearchType) => Promise<void>;
 };
 
 export default function Form({ fetchWeather }: FormProps) {
@@ -24,12 +24,12 @@ export default function Form({ fetchWeather }: FormProps) {
 
   const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     if (Object.values(search).includes('')) {
       setAlert('Todos los campos son obligatorios');
       return;
     }
-    fetchWeather();
+    fetchWeather(search);
   };
 
   return (
