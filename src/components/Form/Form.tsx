f6d21a9f4945dc494d9f5965230730ef145e65e1
Andrés Muñoz <andres2078@gmail.com>
2026-08-12 14:00:04 -0500

    feat: add form validation and alert component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,15 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent, type SubmitEvent } from 'react';
 import type { SearchType } from '../../types';
 import { countries } from '../../data/countries';
 import styles from './Form.module.css';
+import Alert from '../Alert/Alert';
 
 export default function Form() {
   const [search, setSearch] = useState<SearchType>({
     city: '',
     country: '',
   });
+  const [alert, setAlert] = useState('')
 
   const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     setSearch({
@@ -16,8 +18,21 @@ export default function Form() {
     });
   };
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (Object.values(search).includes('')) {
+      setAlert('Todos los campos son obligatorios')
+      return
+    }
+  }
+
   return (
-    <form className={styles.form}>
+    <form 
+      className={styles.form}
+      onSubmit={handleSubmit}
+    >
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input
