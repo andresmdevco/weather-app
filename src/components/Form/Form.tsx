e408730f8b265f5e152b9554f12c1b5f048d5b35
Andrés Muñoz <andres2078@gmail.com>
2026-08-12 10:42:13 -0500

    style: add form styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,14 +1,15 @@
 import { countries } from '../../data/countries';
+import styles from './Form.module.css'
 
 export default function Form() {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input id="city" type="text" name="city" placeholder="Ciudad" />
       </div>
 
-      <div>
+      <div className={styles.field}>
         <label htmlFor="city">País:</label>
         <select name="" id="">
           <option value="">--- Seleccione un País ---</option>
@@ -20,7 +21,7 @@ export default function Form() {
         </select>
       </div>
 
-      <input type="submit" value="Consultar Clima" />
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
     </form>
   );
 }
